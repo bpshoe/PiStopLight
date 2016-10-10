269b86aa4e265a3f5620df5f4b61414db25e13a1
bpshoe <bpshoe@gmail.com>
2016-10-10 12:10:06 -0400

    fixed order

--- stoplight.py
+++ stoplight.py
@@ -8,13 +8,13 @@ GPIO.setup(22, GPIO.OUT)
 import time
 
 while True:
-	GPIO.output(17, 1)
-	time.sleep(5)
-	GPIO.output(17, 0)
-	GPIO.output(27, 1)
-	time.sleep(2)
-	GPIO.output(27, 0)
 	GPIO.output(22, 1)
 	time.sleep(5)
 	GPIO.output(22, 0)
+	GPIO.output(27, 1)
+	time.sleep(2)
+	GPIO.output(27, 0)
+	GPIO.output(17, 1)
+	time.sleep(5)
+	GPIO.output(17, 0)
 
