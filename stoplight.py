4f9775c2f0170eb1c409933173b50f7fd9cc304c
bpshoe <bpshoe@gmail.com>
2016-10-10 11:56:10 -0400

    added stoplight first run

---
+++ stoplight.py
@@ -0,0 +1,18 @@
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(17, GPIO.OUT)
+GPIO.setup(27, GPIO.OUT)
+GPIO.setup(22, GPIO.OUT)
+
+while True:
+	GPIO.output(17, 1)
+	time.sleep(5)
+	GPIO.output(17, 0)
+	GPIO.output(27, 1)
+	time.sleep(2)
+	GPIO.output(27, 0)
+	GPIO.output(22, 1)
+	time.sleep(5)
+	GPIO.output(22, 0)
+
